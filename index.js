e232588ddaaa1f49ab97d1b8643569f54bd34d8e
bruno <brunoukomadu1@gmail.com>
2026-03-21 10:02:52 +0100

    added background and writtings also work on viewpoint

---
+++ index.js
@@ -0,0 +1,46 @@
+let firstName = "kim" ;
+let lastName = "Jhon" ;
+let fullname = firstName + lastName ;
+console.log("my fullname is",fullname)
+
+let person = {
+    firstName: "Jhon",
+    lastName: "Stephen",
+    age: 20,
+    gender: "Male",
+};
+console.log(person)
+console.log(person.firstName)
+console.log(person.age)
+
+let book = {
+    author :{
+        Name: "Emman Jons",
+        country: "Cameroon",
+        gender: "Male",
+    },
+    title: "Learn to code",
+    Published: "2018",
+}
+console.log(book)
+console.log("bookauthor", book.author)
+console.log("authorname:", book.author.Name)
+
+let student = {
+    "name": "Paul",
+    "class": "three",
+    "school":{
+        "Name":"G.H.S",
+        "Location": "limbe",
+
+    }
+}
+console.log(student)
+
+let kingblack = {
+    "courses" : {
+        "programing": "1year",
+        "digitalmarketing": "10months"
+    }
+}
+console.log(kingblack)
