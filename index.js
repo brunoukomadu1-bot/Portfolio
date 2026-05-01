eeec9fdd0c3d60229cacc79035eaeb455a98ca34
bruno <brunoukomadu1@gmail.com>
2026-05-01 12:54:59 +0100

    add some other projects

--- index.js
+++ index.js
@@ -1,46 +1,353 @@
-let firstName = "kim" ;
-let lastName = "Jhon" ;
-let fullname = firstName + lastName ;
-console.log("my fullname is",fullname)
+// let firstName = "kim" ;
+// let lastName = "Jhon" ;
+// let fullname = firstName + lastName ;
+// console.log("my fullname is",fullname)
 
-let person = {
-    firstName: "Jhon",
-    lastName: "Stephen",
-    age: 20,
-    gender: "Male",
-};
-console.log(person)
-console.log(person.firstName)
-console.log(person.age)
+// let person = {
+//     firstName: "Jhon",
+//     lastName: "Stephen",
+//     age: 20,
+//     gender: "Male",
+// };
+// console.log(person)
+// console.log(person.firstName)
+// console.log(person.age)
 
-let book = {
-    author :{
-        Name: "Emman Jons",
-        country: "Cameroon",
-        gender: "Male",
-    },
-    title: "Learn to code",
-    Published: "2018",
+// let book = {
+//     author :{
+//         Name: "Emman Jons",
+//         country: "Cameroon",
+//         gender: "Male",
+//     },
+//     title: "Learn to code",
+//     Published: "2018",
+// }
+// console.log(book)
+// console.log("bookauthor", book.author)
+// console.log("authorname:", book.author.Name)
+
+// let student = {
+//     "name": "Paul",
+//     "class": "three",
+//     "school":{
+//         "Name":"G.H.S",
+//         "Location": "limbe",
+
+//     }
+// }
+// console.log(student)
+
+// let kingblack = {
+//     "courses" : {
+//         "programing": "1year",
+//         "digitalmarketing": "10months"
+//     }
+// }
+// console.log(kingblack)
+
+// Functions
+// function helloWorld() {
+//     let text ="hello World"
+//     console.log(text)
+// }
+
+// helloWorld()
+
+// function four() {
+//     let number = 4
+//     console.log(number)
+// }
+
+// four()
+
+// function add
+
+const data = [
+  {
+    detailUrl: '#_react',
+    liveUrl: 'https://betxxx.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/sportsbook.git',
+    img: 'images/works-images/sportsbook-img.PNG',
+    name: 'Sportsbook',
+    desc: `A responsive sports betting app where users can signup, signin, place bets, deposit/withdraw funds`,
+    technologies: ['NextJS', 'Redux', 'TypeScript', 'mongoDB'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_react',
+    liveUrl: 'https://becuda-buea-demo.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/becuda-buea.git',
+    img: 'images/works-images/becuda-buea-demo.PNG',
+    name: 'Finance management app',
+    desc: ` A comprehensive financial management app designed to simplify personal finances by offering 
+    seamless tools for saving, withdrawing, borrowing, and managing fines. Users can track their savings
+     growth and keep a record of any imposed fines`,
+    technologies: ['NextJS', 'Redux', 'TypeScript', 'mongoDB'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_react',
+    liveUrl: 'https://musong-task-manager.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/task-manager',
+    img: 'images/works-images/task-m.jpg',
+    name: 'Task manager',
+    desc: `A robust solution for individuals or teams seeking an efficient and
+    visually pleasing way to manage their tasks, fostering productivity and organization.`,
+    technologies: ['ReactJS', 'Redux', 'Bootstrap'],
+    skills: ['ReactJS', 'Redux', 'Bootstrap'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_react',
+    liveUrl: 'https://musong-furniture-store.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-House-Furniture-Store',
+    img: 'images/works-images/furniture-store.png',
+    name: 'furniture store',
+    desc: `An E-commerce Web App that enables users to buy online with the
+    following features: Add cart items, Remove cart items, Clear cart,
+    Calculate tax, Sub totals, and totals.`,
+    technologies: ['React', 'Redux', 'Bootstrap'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_rails',
+    liveUrl: 'https://becuda.netlify.app/#/',
+    sourseUrl: 'https://github.com/ernestmusong/Cultural-website',
+    img: 'images/works-images/becuda.png',
+    name: 'Cultural website',
+    desc: `A website created to manage the activities of my cultural association. Users can make contributions, keep track of their contributions and see records of all contributions of my cultural association.`,
+    technologies: ['React', 'HTML', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+
+  {
+    detailUrl: '#_bootstrap',
+    liveUrl: '/contribute-page',
+    sourseUrl: 'https://github.com/ernestmusong/Annual-Income-Report',
+    img: 'images/works-images/income-statement.png',
+    name: 'Annual Income Report',
+    desc: `Pulls from the Financial modeling prep API to give annual report
+    of the income statements for the company "Apple Inc." for the past
+    thirty-eight years.`,
+    technologies: ['React', 'Redux', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_rails',
+    liveUrl: 'https://musong-bookstore.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/Bookstore',
+    img: 'images/works-images/bookstore.png',
+    name: 'Bookstore',
+    desc: `Pulls from the Bookstore api to display a
+    list of books, with the following features: Add,
+    Remove, and Edit.`,
+    technologies: ['React', 'Redux', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_rails',
+    liveUrl: 'https://musong-math-magicians.onrender.com/',
+    sourseUrl: 'https://github.com/ernestmusong/Math-magicians',
+    img: 'images/works-images/React-calculator.PNG',
+    name: 'math magicians',
+    desc: `A single page application containing three pages: a welcome
+    page, a calculator, and a quote page. The quote page pulls from the
+    Ninja API and displays a random quote. The calculator component 
+    contains a calculator that performs mathematical operations.`,
+    technologies: ['React', 'APIs', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_rails',
+    liveUrl: 'https://musong-react-pomodoro-clock.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-Pomodoro-clock',
+    img: 'images/works-images/pomo.png',
+    name: 'pomodoro clock',
+    desc: `Pomodoro clock is a timer clock built with React that has 
+    a break length of 5 minutes and a session length of 25 minutes by 
+    default. User can set the break and session lengths by clicking 
+    the (+) or (-) keys, play and pause the clock. The end of every 
+    session is indicated by a sound produced by the machine. `,
+    technologies: ['React', 'HTML', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    detailUrl: '#_rails',
+    liveUrl: 'https://ernest-markdown-previewer.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-Markdown-Previewer',
+    img: 'images/works-images/markdown.png',
+    name: 'markdown previewer',
+    desc: `Markdown previewer is a tool that allows you to write and preview 
+    Markdown-formatted text in real-time. It allows you to format text using 
+    simple syntax, such as adding asterisks around words to make them bold, 
+    or adding hash marks to create headings.`,
+    technologies: ['React', 'HTML', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+];
+
+
+// function getData() {
+ 
+//   const detailUrls = data.map((item)=>{
+//     let detailUrl = item.detailUrl
+//     // console.log(item)
+//     return detailUrl
+//   })
+//   console.log(detailUrls)
+//   return detailUrls
+// }
+// getData()
+
+function geturl() {
+  const liveUrls = data.map((T)=>{
+    let liveUrl = T.liveUrl
+    return liveUrl
+  })
+  console.log(liveUrls)
+  return liveUrls
 }
-console.log(book)
-console.log("bookauthor", book.author)
-console.log("authorname:", book.author.Name)
+geturl()
 
-let student = {
-    "name": "Paul",
-    "class": "three",
-    "school":{
-        "Name":"G.H.S",
-        "Location": "limbe",
-
-    }
+function getimg() {
+  const getimgs = data.map((I)=>{
+    let img = I.img
+    return img
+  })
+  console.log(getimgs)
+  return getimgs
 }
-console.log(student)
+getimg()
 
-let kingblack = {
-    "courses" : {
-        "programing": "1year",
-        "digitalmarketing": "10months"
-    }
+function getname(){
+  const getnames = data.map((N)=>{
+    let name = N.name
+    return name
+  })
+  console.log(getnames)
+  return getname
 }
-console.log(kingblack)
+getname()
+
+
+function getskill(){
+  const getskills = data.map((R)=>{
+    let skill = R.skill
+    return skill
+  })
+  console.log(getskills)
+  return getskill
+}
+getskill()
+
+
+function getseeLiveIcon(){
+  const getseeLiveIcons = data.map((L)=>{
+    let seeLiveIcon = L.seeLiveIcon
+    return seeLiveIcon
+  })
+  console.log(getseeLiveIcons)
+  return getseeLiveIcon
+}
+getseeLiveIcon()
+
+function getseeSourceIcon() {
+  const getseeSourceIcons = data.map((A)=>{
+    let seeSourceIcon = A.seeSourceIcon
+    return seeSourceIcon
+  })
+  console.log(getseeSourceIcons)
+  return getseeSourceIcon
+}
+getseeSourceIcon()
+
+
+function getmobilePopUpClose(){
+  const getmobilePopUpCloses = data.map((P)=>{
+    let mobilePopUpClose =P.mobilePopUpClose
+    return mobilePopUpClose
+  })
+  console.log(getmobilePopUpCloses)
+  return getmobilePopUpClose
+}
+getmobilePopUpClose()
+
+
+function getdesc(){
+  const getdescs = data.map((D)=>{
+    let desc = D.desc
+    return desc
+  })
+  console.log(getdescs)
+  return getdesc
+}
+getdesc()
+
+function gettechnologies(){
+  const gettechnologies = data.map((G)=>{
+    let technologies = G.technologies
+    return technologies 
+  })
+  console.log(gettechnologies)
+  return gettechnologies
+}
+gettechnologies()
+
+function getsourseUrl(){
+  const getsourseUrls = data.map((C)=>{
+    let sourseUrl = C.sourseUrl
+    return sourseUrl
+  })
+  console.log(getsourseUrls)
+  return getsourseUrl
+}
+getsourseUrl()
