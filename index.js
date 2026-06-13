38a6f4717ffbc66da90fb24a5c9f6a2ff68a0f36
bruno <brunoukomadu1@gmail.com>
2026-06-12 19:50:21 -1100

    added more projects

--- index.js
+++ index.js
@@ -64,176 +64,33 @@
 
 const data = [
   {
-    detailUrl: '#_react',
     liveUrl: 'https://betxxx.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/sportsbook.git',
-    img: 'images/works-images/sportsbook-img.PNG',
-    name: 'Sportsbook',
-    desc: `A responsive sports betting app where users can signup, signin, place bets, deposit/withdraw funds`,
-    technologies: ['NextJS', 'Redux', 'TypeScript', 'mongoDB'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
+    sourseUrl: 'https://github.com/brunoukomadu1-bot/js-todo',
+    img: 'images/Todo.png',
+    name: 'Todo app',
+    desc: `conquer your day, one task at a time. Streamline your workflow, set reminders, and crush your goals with todo app`,
   },
   {
-    detailUrl: '#_react',
-    liveUrl: 'https://becuda-buea-demo.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/becuda-buea.git',
-    img: 'images/works-images/becuda-buea-demo.PNG',
-    name: 'Finance management app',
-    desc: ` A comprehensive financial management app designed to simplify personal finances by offering 
-    seamless tools for saving, withdrawing, borrowing, and managing fines. Users can track their savings
-     growth and keep a record of any imposed fines`,
-    technologies: ['NextJS', 'Redux', 'TypeScript', 'mongoDB'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
+    liveUrl: 'https://betxxx.netlify.app/',
+    sourseUrl: 'https://github.com/brunoukomadu1-bot/Counter-app',
+    img: 'images/Counter.png',
+    name: 'Java script counter',
+    desc: `From 0-10 with just a press`,
   },
   {
-    detailUrl: '#_react',
-    liveUrl: 'https://musong-task-manager.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/task-manager',
-    img: 'images/works-images/task-m.jpg',
-    name: 'Task manager',
-    desc: `A robust solution for individuals or teams seeking an efficient and
-    visually pleasing way to manage their tasks, fostering productivity and organization.`,
-    technologies: ['ReactJS', 'Redux', 'Bootstrap'],
-    skills: ['ReactJS', 'Redux', 'Bootstrap'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
+    liveUrl: 'https://betxxx.netlify.app/',
+    sourseUrl: 'https://github.com/brunoukomadu1-bot/Search-site',
+    img: 'images/search section.png',
+    name: 'Search Section',
+    desc: `Search anything you wish`,
   },
-  {
-    detailUrl: '#_react',
-    liveUrl: 'https://musong-furniture-store.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/React-House-Furniture-Store',
-    img: 'images/works-images/furniture-store.png',
-    name: 'furniture store',
-    desc: `An E-commerce Web App that enables users to buy online with the
-    following features: Add cart items, Remove cart items, Clear cart,
-    Calculate tax, Sub totals, and totals.`,
-    technologies: ['React', 'Redux', 'Bootstrap'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
-    detailUrl: '#_rails',
-    liveUrl: 'https://becuda.netlify.app/#/',
-    sourseUrl: 'https://github.com/ernestmusong/Cultural-website',
-    img: 'images/works-images/becuda.png',
-    name: 'Cultural website',
-    desc: `A website created to manage the activities of my cultural association. Users can make contributions, keep track of their contributions and see records of all contributions of my cultural association.`,
-    technologies: ['React', 'HTML', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-
-  {
-    detailUrl: '#_bootstrap',
-    liveUrl: '/contribute-page',
-    sourseUrl: 'https://github.com/ernestmusong/Annual-Income-Report',
-    img: 'images/works-images/income-statement.png',
-    name: 'Annual Income Report',
-    desc: `Pulls from the Financial modeling prep API to give annual report
-    of the income statements for the company "Apple Inc." for the past
-    thirty-eight years.`,
-    technologies: ['React', 'Redux', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
-    detailUrl: '#_rails',
-    liveUrl: 'https://musong-bookstore.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/Bookstore',
-    img: 'images/works-images/bookstore.png',
-    name: 'Bookstore',
-    desc: `Pulls from the Bookstore api to display a
-    list of books, with the following features: Add,
-    Remove, and Edit.`,
-    technologies: ['React', 'Redux', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
-    detailUrl: '#_rails',
-    liveUrl: 'https://musong-math-magicians.onrender.com/',
-    sourseUrl: 'https://github.com/ernestmusong/Math-magicians',
-    img: 'images/works-images/React-calculator.PNG',
-    name: 'math magicians',
-    desc: `A single page application containing three pages: a welcome
-    page, a calculator, and a quote page. The quote page pulls from the
-    Ninja API and displays a random quote. The calculator component 
-    contains a calculator that performs mathematical operations.`,
-    technologies: ['React', 'APIs', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
-    detailUrl: '#_rails',
-    liveUrl: 'https://musong-react-pomodoro-clock.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/React-Pomodoro-clock',
-    img: 'images/works-images/pomo.png',
-    name: 'pomodoro clock',
-    desc: `Pomodoro clock is a timer clock built with React that has 
-    a break length of 5 minutes and a session length of 25 minutes by 
-    default. User can set the break and session lengths by clicking 
-    the (+) or (-) keys, play and pause the clock. The end of every 
-    session is indicated by a sound produced by the machine. `,
-    technologies: ['React', 'HTML', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
-    detailUrl: '#_rails',
-    liveUrl: 'https://ernest-markdown-previewer.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/React-Markdown-Previewer',
-    img: 'images/works-images/markdown.png',
-    name: 'markdown previewer',
-    desc: `Markdown previewer is a tool that allows you to write and preview 
-    Markdown-formatted text in real-time. It allows you to format text using 
-    simple syntax, such as adding asterisks around words to make them bold, 
-    or adding hash marks to create headings.`,
-    technologies: ['React', 'HTML', 'CSS'],
-    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
+  
 ];
+
+const name = document.querySelector(".project-title")
+const desc = document.querySelector("#desc")
+console.log(name)
+
 
 
 // function getData() {
